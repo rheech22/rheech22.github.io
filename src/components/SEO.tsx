0d7d09d42bfa79f4cdd694f7652164321aed823a
Aiden <rheech22@gmail.com>
2022-09-25 00:26:08 +0900

    Favicon

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -33,8 +33,8 @@ const SEO = ({ subTitle, description, pathname, children }: Props) => {
       <meta name="twitter:description" content={seo.description} />
       <meta name="twitter:image" content={seo.image} />
       <meta name="twitter:creator" content={seo.twitterUsername} />
-      <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='0.9em' font-size='90'></text></svg>" />
-      <link rel="apple-touch-icon" href="../images/favicon.png" />
+      <link rel="icon" type="image/png" href="../images/favicon.png" />
+      <link rel="apple-touch-icon" href="../images/icon180.png" />
       {children}
     </>
   );
