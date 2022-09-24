7682fc4044103fc29fa70184e2b0065cde3ce769
Aiden <rheech22@gmail.com>
2022-09-25 04:00:54 +0900

    Fix

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,16 +1,3 @@
 import wrapper from './wrapPageElement';
 
 export const wrapPageElement = wrapper;
-
-export const onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    'This application has been updated. ' +
-      'Reload to display the latest version?'
-  );
-
-  if (answer === true) {
-    window.location.reload();
-  }
-};
-
-export const registerServiceWorker = () => true;
