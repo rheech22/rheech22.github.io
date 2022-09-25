00b888d113e157c0fd2052e7939290fadfa15f08
Aiden <rheech22@gmail.com>
2022-09-25 17:52:18 +0900

    SW

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,3 +1,14 @@
 import wrapper from './wrapPageElement';
 
 export const wrapPageElement = wrapper;
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    'This application has been updated. ' +
+      'Reload to display the latest version?'
+  );
+
+  if (answer === true) {
+    window.location.reload();
+  }
+};
