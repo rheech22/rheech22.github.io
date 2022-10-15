c9ff70b546bddca1c3a008ed1a6021971a9545d4
Aiden <rheech22@gmail.com>
2022-10-15 22:58:52 +0900

    Body width

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -20,7 +20,7 @@ const globalStyle = createGlobalStyle<GlobalStyle>`
   }
   
   body {
-    width: 100vw;
+    width: 100%;
     background-color: ${({ theme }) => (theme.bg)};
     color: ${({ theme }) => (theme.default)};
     min-height: 100vh;
